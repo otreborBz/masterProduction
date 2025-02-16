028a88bb1db5af127112823219b7db123b0f80cc
unknown <rlcarval38@gmail.com>
2025-02-16 18:20:46 -0300

    finalizado v1.0

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence, getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoginScreen from './screens/LoginScreen';
 import ProductionDataScreen from './screens/ProductionDataScreen';
@@ -15,27 +15,16 @@ const Stack = createNativeStackNavigator();
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyASES_8k2Grx0Zo3za5Z0mJuZxoEkSAKio",
-  authDomain: "production-f18dd.firebaseapp.com",
-  projectId: "production-f18dd",
-  storageBucket: "production-f18dd.firebasestorage.app",
-  messagingSenderId: "953263823099",
-  appId: "1:953263823099:web:a1beb9e56f7e2aedd4385e",
-  measurementId: "G-YXRCWH0LVR"
+  // colocar as configuracoes do firebase
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
 // Inicializar Auth com persistência
-let auth;
-try {
-  auth = getAuth(app);
-} catch (error) {
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-  });
-}
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
